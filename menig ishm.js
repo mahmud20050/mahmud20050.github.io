9c31eb756f2383c36ad05dcee8b8fb240b93e68a
mahmud20050 <104900211+mahmud20050@users.noreply.github.com>
2022-07-30 16:26:15 +0500

    C:\Users\Professional\Documents\GitHub\studentwebuzb.github.io\mahmudjon\uzb.css

--- menig ishm.js
+++ menig ishm.js
@@ -1,13 +1,10 @@
+let field = document.querySelector('.items');
+let li = Array.from(field.children)
+let select = document.getElementById("select");
+let ar = [];
 
-
-let field =document.querySelector('.items');
-let li =Array.from(field.children)
-let select =document.getElementById("select");
-let ar =[];
-
-for(let i of li)
-{
-    const last =i.lastElementChild;
+for (let i of li) {
+    const last = i.lastElementChild;
     const x = last.textContent.trim();
     const y = Number(x.substring(1));
     i.setAttribute('data-price', y);
